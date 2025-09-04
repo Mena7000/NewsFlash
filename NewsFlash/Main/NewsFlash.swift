a03ee21a9349608d3a017a28603084fe0c32c5d3
Mina Maher <36644182+Mena7000@users.noreply.github.com>
2025-09-04 13:47:09 +0300

    removed all SceneManifest related and now pure SwiftUI project

---
+++ NewsFlash/Main/NewsFlash.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFlashApp.swift
+//  NewsFlash
+//
+//  Created by Mena Maher on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsFlash: App {
+    var body: some Scene {
+        WindowGroup {
+            TopHeadlineUI()
+        }
+    }
+}
